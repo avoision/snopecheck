2b5eb4bc18ac0f02e2e9a244fbb2bc56e79efaf6
avoision <felix@avoision.com>
2016-03-15 20:37:00 -0500

    Initial commit.

---
+++ snopecheck.js
@@ -0,0 +1,22 @@
+var text = $('._6m6 a').first().text();
+console.log("I found: " + text);
+
+var urlprefix = 'http://www.snopes.com/search/?q=';
+// var urlprefix = 'https://www.google.com/search?q=';
+
+var searchURL = urlprefix + text;
+
+
+// ===========================
+// Snopes
+// ===========================
+	// #search-box
+		// .search-results
+			// .item 
+				// .text
+					// h3.title
+						// spans
+
+
+
+console.log('--------------------------- end of line ---------------------------  ');
